8b363db6b224c8bcacae449dbab41f2eb89b5f83
tkm <sumikawa.web@gmail.com>
2020-09-30 20:16:38 +0900

    postal-code

---
+++ postal-code/script.js
@@ -0,0 +1,26 @@
+$(function () {
+  $('#btn').on('click', function () {
+    $.ajax({
+      url:
+        'http://zipcloud.ibsnet.co.jp/api/search?zipcode=' +
+        $('#postcode').val(),
+      dataType: 'jsonp',
+    })
+      .done((data) => {
+        if (data.results) {
+          getData(data.results[0]);
+        } else {
+          alert('該当データが見つかりません');
+        }
+      })
+      .fail((data) => {
+        alert('通信に失敗しました');
+      });
+  });
+
+  function getData(data) {
+    $('#pref').val(data.address1);
+    $('#city').val(data.address2);
+    $('#address').val(data.address3);
+  }
+});
